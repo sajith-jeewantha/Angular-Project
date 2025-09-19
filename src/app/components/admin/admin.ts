59098746d77e08eb7c69ddb1d91650be085c037f
Sajith Jeewantha <112331315+sajith-jeewantha@users.noreply.github.com>
2025-09-19 22:45:52 +0530

    test1

---
+++ src/app/components/admin/admin.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css',
+})
+export class Admin {
+  author = "Sajith"
+  project = "Angilar 20"
+  version= 1.0
+}
