52f571f6b30b1a8075a604ae1f89d13fb98b8d53
Sajith Jeewantha <112331315+sajith-jeewantha@users.noreply.github.com>
2025-09-21 22:47:48 +0530

    Post API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { DataBinding } from './components/data-binding/data-binding';
 import { SignalEx } from './components/signal-ex/signal-ex';
 import { AttDirective } from './components/att-directive/att-directive';
 import { GetApi } from './components/get-api/get-api';
+import { User } from './components/user/user';
 
 export const routes: Routes = [
 
@@ -31,6 +32,9 @@ export const routes: Routes = [
     {
         path: 'get-api',
         component: GetApi
+    },{
+        path: 'user',
+        component: User
     }
 
 ];
--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule, NgModel } from "@angular/forms";
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
+export class User implements OnInit {
+
+  http = inject(HttpClient);
+  userList: any[] = [];
+
+  userObject: any = {
+    "userId": 0,
+    "emailId": "",
+    "password": "",
+    "fullName": "",
+    "mobileNo": ""
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://api.freeprojectapi.com/api/GoalTracker/getAllUsers')
+      .subscribe((data: any) => {
+        this.userList = data;
+      });
+  }
+
+  onUserSave() {
+    this.http.post('https://api.freeprojectapi.com/api/GoalTracker/register', this.userObject)
+      .subscribe({
+        next: (result) => {
+          debugger
+          alert('User Registered Successfully');
+          this.getUsers();
+        },
+        error: (error) => {
+          debugger
+          alert('Error Occurred - '+error.error) },
+      })
+  }
 
 }
